4357587404d0d8b27b306a4866b3f2f155cdffdc
Yura Bazaliy <yurchykxxx@gmail.com>
2016-04-12 20:45:59 +0300

    Editing booking layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,11 @@
 $('select').selectpicker({
-  width: 'auto'
+  width: '100%',
+  liveSearch: true
+});
+
+$('.input-daterange').datepicker({
+
+  startDate: 'today',
+  language: $('html').attr('lang')
+
 });
